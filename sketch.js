5a3eee03fbbb551b3400254ee2a066eaa6815d37
katiexhancock <katie01225@gmail.com>
2023-03-08 21:38:46 -0800

    Fixed some bugs

--- sketch.js
+++ sketch.js
@@ -1,67 +1,124 @@
 let c = "black";
 let eraserMode = false;
 let slider = 10; // initial size of pen and eraser
+let colors = ["black", "gray", "white", "red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+menuHeight = 60;
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(windowWidth, windowHeight);
   background(255);
+  saveState();
 
   // Draw a black outline around the canvas
   noFill();
-  stroke(0);
+  stroke(200);
   strokeWeight(1);
   rect(0, 0, width, height);
 
   // Add a menu bar
   fill(200);
-  rect(0, 0, width, 40);
+  rect(0, 0, width, menuHeight);
 
   // Add a "Save" button to the menu bar
-  fill(255);
-  rect(width-60, 5, 50, 30);
-  fill(0);
-  textSize(12);
-  textAlign(CENTER, CENTER);
-  text("Save", width-35, 20);
+  saveButton = createButton("Save");
+  saveButton.position(width-55, 20);
+  saveButton.class("button");
+  saveButton.mousePressed(function() {
+     // Create a new graphics object to hold the portion of the canvas we want to save
+    let graphics = createGraphics(width, height-menuHeight);
+
+    // Draw the portion of the canvas we want to save onto the graphics object
+    graphics.image(get(0, menuHeight, width, height-menuHeight), 0, menuHeight);
+
+    // Save the graphics object as a PNG file
+    saveCanvas(graphics, 'myDrawing', 'png');
+  });
 
   // Add a "Clear" button to the menu bar
-  fill(255);
-  rect(10, 5, 50, 30);
-  fill(0);
-  textSize(12);
-  textAlign(CENTER, CENTER);
-  text("Clear", 35, 20);
+  clearButton = createButton("Clear");
+  clearButton.position(10, 5);
+  clearButton.class("button");
+  clearButton.mousePressed(function() {
+    // Display a confirmation popup
+    var confirmed = confirm("Are you sure you want to clear your drawing? This will erase all your previous work.");
+    // If the user clicks "OK", clear the canvas
+    if (confirmed) {
+      // Clear only the area below the menu bar
+      clearDrawing();
+    }
+  });
 
   // Add an "Eraser" button to the menu bar
-  fill(255);
-  rect(70, 5, 50, 30);
-  fill(0);
-  text("Eraser", 95, 20);
+  eraserButton = createButton("Eraser");
+  eraserButton.position(85, 5);
+  eraserButton.class("button");
+  eraserButton.mousePressed(function() {
+    eraserMode = true;
+  });
 
   // Add a "Pen" button to the menu bar
-  fill(255);
-  rect(130, 5, 50, 30);
-  fill(0);
-  text("Pen", 155, 20);
+  penButton = createButton("Pen");
+  penButton.position(155, 5);
+  penButton.class("button");
+  penButton.mousePressed(function() {
+      eraserMode = false;
+    });
 
   // Add a color panel to the menu bar
-  let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-  let startX = 200;
+  let startX = 220;
   let circleSize = 20;
+
+  // Draw the color circles
   for (let i = 0; i < colors.length; i++) {
     let colorX = startX + (i * circleSize * 1.5);
-    fill(colors[i]);
-    ellipse(colorX, 20, circleSize, circleSize);
+    colorButton = createButton("");
+    colorButton.position(colorX, 10);
+    colorButton.style('background-color', colors[i]);
+    colorButton.style('border-radius', '50%');
+    colorButton.style('height', '20px');
+    colorButton.style('width', '20px');
+    colorButton.style('border', 'none');
+    colorButton.class("button");
+    colorButton.mousePressed(function() {
+      eraserMode = false;
+      colorButtonClicked(colors[i]);
+    });
   }
+
+  // Writing the colors text
+  let pickerX = startX + (colors.length / 4 * circleSize * 1.5);
+  stroke(0);
+  strokeWeight(1);
+  fill(0);
+  textAlign(CENTER, CENTER);
+  textSize(12);
+  text("Colors", pickerX + 50, 45);
 
   // Add a size slider to the menu bar
   slider = createSlider(1, 50, slider);
-  slider.position(400, 10);
+  slider.position(60, 40);
+
+  // Adding a color picker
+  colorPicker = createColorPicker(c);
+  colorPicker.position(550, 10);
+  colorPicker.class("button");
+  colorPicker.input(handleColorInput);
+  textSize(10);
+  text("Color Picker", 575, 50);
 }
 
+function colorButtonClicked(buttonColor) {
+  c = buttonColor;
+  stroke(buttonColor);
+}
+
+function handleColorInput() {
+  c = this.value();
+}
 
 function draw() {
-  if (mouseIsPressed && mouseY > 40) { // Check if mouse is inside canvas
+  colorPicker.value(c);
+  if (mouseIsPressed && mouseY > menuHeight + slider.value() / 2) { // Check if mouse is inside canvas
     if (eraserMode) {
       stroke(255); // Use white for eraser
     } else {
@@ -109,21 +166,6 @@ function mousePressed() {
     eraserMode = false;
   }
 
-  // Check if color panel is clicked
-  if (mouseY < 40) {
-    let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
-    let startX = 200;
-    let circleSize = 20;
-    for (let i = 0; i < colors.length; i++) {
-      let colorX = startX + (i * circleSize * 1.5);
-      if (mouseX > colorX - circleSize / 2 && mouseX < colorX + circleSize / 2) {
-        c = colors[i];
-        eraserMode = false; // Set eraser mode to false when a color is selected
-        stroke(c); // Set stroke color to selected color for pen
-      }
-    }
-  }
-
   // Draw with pen or eraser depending on mode
   if (eraserMode) {
     stroke(255); // Use white for eraser
@@ -137,7 +179,15 @@ function mousePressed() {
 }
 
 function clearDrawing() {
-  // Clear only the area below the menu bar
-  fill(255);
-  rect(0, 40, width, height-40);
+    // Clear only the area below the menu bar
+    fill(255);
+    stroke(200);
+    strokeWeight(1);
+    rect(0, menuHeight, width, height);
+}
+
+function saveState() {
+  // save state by taking image of background
+  // for more info look at reference for get
+  previousState = get();
 }
